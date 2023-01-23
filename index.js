6b085656d209691254d9112781bcd888b9c98e06
diego.viador <dviadorf@fpcoruna.afundacion.org>
2023-01-23 18:10:56 +0100

    Add Html files through file system Node package

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const http = require('http') //Require, similar a import o include, para incluir paquetes de node. Se pasa el nombre del paquete como una string
+//Require, similar a import o include, para incluir paquetes de node. Se pasa el nombre del paquete como una string
+const http = require('http') // Este paquete sirve para manejar req y res de http y realizar acciones en el server.
+const fs = require('fs') //Este paquete nos permite interactuar con archivos de nuestro servidor. File System
+
+// Usamos el paquete fs con el metodo readFile para cargar los html en variables
+const homePage = fs.readFileSync('./html/index.html')
+const aboutPage = fs.readFileSync('./html/about.html')
+const contactPage = fs.readFileSync('./html/contact.html')
+const notFoundPage = fs.readFileSync('./html/notfound.html')
+
 /**
+ * @name RequestHandler
  * Función callback. Espera una request y enviará la repsuesta sólo cuando la reciba.
- * En este caso sacará por consola en noe la url de la request e imprimira texto en la web como repsuesta.
+ * En este caso sacará por consola en noe la url de la request e imprimira texto en la web como respuesta.
+ * Todas las APIs node tiene una request handler core
  */
 const server = http.createServer((req, res) => {
     console.log(req.url)
     if (req.url === '/') {
-        res.end('This is the home page')
+        res.end(homePage)
     } else if (req.url === '/about') {
-        res.end('This is the about page')
+        res.end(aboutPage)
     } else if (req.url === '/contact') {
-        res.end('This is the contact page')
+        res.end(contactPage)
     } else {
         /**
-         * Escribe el staus de la request. 
+         * Escribe el status de la request. 
          * Se puede ver desde el Network del Inspeccionar del navegador. Por defecto es 200.
          */
         res.writeHead(404)
-        res.end('Page not found')
+        res.end(notFoundPage)
     }
     
 })
