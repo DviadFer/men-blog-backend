cd63aa2afd4b62524b3c79a088e475b969824d15
DviadFer <diegoviadfer@gmail.com>
2023-01-24 18:01:44 +0100

    Modify index.js to use Express package instead of http, fs and req/res objects.

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-//Require, similar a import o include, para incluir paquetes de node. Se pasa el nombre del paquete como una string
-const http = require('http') // Este paquete sirve para manejar req y res de http y realizar acciones en el server.
-const fs = require('fs') //Este paquete nos permite interactuar con archivos de nuestro servidor. File System
+const express = require('express') //hace uso del paquete express instalado con npm
 
-// Usamos el paquete fs con el metodo readFile para cargar los html en variables
-const homePage = fs.readFileSync('./html/index.html')
-const aboutPage = fs.readFileSync('./html/about.html')
-const contactPage = fs.readFileSync('./html/contact.html')
-const notFoundPage = fs.readFileSync('./html/notfound.html')
+const app = express() //llama a la función express para empezar una nueva app Express.
+
+app.listen(3000, ()=>{
+    console.log('Aplicación creada a traves de Express con puerto 3000')
+}) 
 
 /**
- * @name RequestHandler
- * Función callback. Espera una request y enviará la repsuesta sólo cuando la reciba.
- * En este caso sacará por consola en noe la url de la request e imprimira texto en la web como respuesta.
- * Todas las APIs node tiene una request handler core
+ * Express se hace cargo del paquete http para gestionar  los objectos request y response. 
+ * La función callback en este caso, se incluye como segundo argumento de la función app.listen() cuando se inicia
+ * el servidor. 
  */
-const server = http.createServer((req, res) => {
-    console.log(req.url)
-    if (req.url === '/') {
-        res.end(homePage)
-    } else if (req.url === '/about') {
-        res.end(aboutPage)
-    } else if (req.url === '/contact') {
-        res.end(contactPage)
-    } else {
-        /**
-         * Escribe el status de la request. 
-         * Se puede ver desde el Network del Inspeccionar del navegador. Por defecto es 200.
-         */
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }
-    
-})
-
-server.listen(3000) //Puerto de escucha
