34364e02cea80838048912c14863006635d751ba
DviadFer <diegoviadfer@gmail.com>
2023-03-02 18:41:53 +0100

    Add tip comment about ...  usage in js

--- index.js
+++ index.js
@@ -74,6 +74,10 @@ app.post('/posts/store', (req, res) =>{
     let image = req.files.image
     image.mv(path.resolve(__dirname, 'public/img', image.name), async () =>{
         await BlogPost.create({
+            /**
+             * Poniendo entre corchetes req.body y ... antes de el, pasamos en .create() un nuevo objeto con todo el contenido de ...req.body más todas las propiedades que añadamos.
+             * En este caso le pasamos imagen y string que equivaldrá a la ruta de la imagen
+             */
             ...req.body,
             image: '/img/' + image.name
         }).catch(err => console.log(err))
