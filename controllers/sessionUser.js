fa726774ca6e568de14871cde918dee1d8a4aa57
DviadFer <diegoviadfer@gmail.com>
2023-03-23 17:43:46 +0100

    Add user id to session cookie if logged sucessfully. Also prevent entering 'New Post' if not logged.

--- controllers/sessionUser.js
+++ controllers/sessionUser.js
@@ -11,6 +11,9 @@ module.exports = (req, res) => {
             bcrypt.compare(req.body.password, user.password, (err, same) => {
                 if (same) {
                     console.log('Login completed');
+                    //Guarda en el id de usuario (en mongo db se expresa como _id) en como campo userId de la cookie 'session' al realizar la petición.
+                    //En esta cookie, userId será compartido entre el navegador y el servidor hasta que sea reiniciada/eliminada.
+                    req.session.userId = user._id
                     res.redirect('/')
                 } else {
                     console.log("Password don't match!")
