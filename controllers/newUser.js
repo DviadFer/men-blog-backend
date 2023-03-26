b587431abc32a57b73c388f40d55a68fb9680c42
DviadFer <diegoviadfer@gmail.com>
2023-03-26 19:03:54 +0200

    Displaying Validation Errors in user register form ejs when render if there's any

--- controllers/newUser.js
+++ controllers/newUser.js
@@ -1,3 +1,7 @@
 module.exports = (req, res) => {
-    res.render('register') 
+    //Mostraremos los errores guardados de sesión si realizamos la petición en storeUser, al recargar el formulario de registro, 
+    //así que necesitamos pasarlos como variable a su respectiva plantilla EJS 
+    res.render('register', {
+        errors: req.session.validationErrors
+    }) 
 }
--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,15 @@ module.exports = async (req, res) => {
     await User.create(req.body, (error, user) => {
         console.log(user, error)
         if (error) {
+            /**
+             * Object.keys(error.errors) nos devuelve los índices del objeto error. En este caso username y password.
+             * Con map.() podmeos acceder al mensaje de error concreto para cada índice:
+             */
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+            console.log(validationErrors);
+
+            //Podemos guardar estos mensajes de errores en nuestras cookies de sesión para mostrarlos luego:
+            req.session.validationErrors = validationErrors 
             return res.redirect('/users/new')
         } 
         res.redirect('/')
