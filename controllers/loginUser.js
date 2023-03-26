b33e90e0231d9ea9c7abd70a4129d59611fac1e5
DviadFer <diegoviadfer@gmail.com>
2023-03-26 21:34:44 +0200

    Add display errors to login form

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,3 +1,18 @@
 module.exports = (req, res) => {
-    res.render('login') 
+    var username = ""
+    var password = ""
+
+    //Datos almacenados en cache del formulario desde storeUser.js. Si no hubo error alguno estarán como indefinidos
+    const data = req.flash('loginData')[0];
+
+    if(typeof data != "undefined"){
+        username = data.username
+        password = data.password
+    }
+
+    res.render('login', {
+        errors: req.flash('error'),
+        username: username,
+        password: password
+    }) 
 }
--- controllers/sessionUser.js
+++ controllers/sessionUser.js
@@ -14,14 +14,17 @@ module.exports = (req, res) => {
                     //Guarda en el id de usuario (en mongo db se expresa como _id) en como campo userId de la cookie 'session' al realizar la petición.
                     //En esta cookie, userId será compartido entre el navegador y el servidor hasta que sea reiniciada/eliminada.
                     req.session.userId = user._id
-                    res.redirect('/')
+                    return res.redirect('/')
                 } else {
-                    console.log("Password don't match!")
+                    req.flash('error', "Password don't match!")
+                    req.flash('postData', req.body)
+                    return res.redirect('/users/login')
                 }
             })
         } else {
-            console.log("User doesn't exist!");
-            res.redirect('/users/login')
+            req.flash('error', "User doesn't exist!")
+            req.flash('postData', req.body)
+            return res.redirect('/users/login')
         }
     })
 
