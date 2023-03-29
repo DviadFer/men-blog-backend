71aa8600e45167faf74ae75f386067c9a321ed47
DviadFer <diegoviadfer@gmail.com>
2023-03-29 19:38:19 +0200

    Add post and post images. Small corrections

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 const BlogPost = require('../models/BlogPost')
 
 module.exports = async (req, res) => {
-    const postList = await BlogPost.find({}).populate('author')
+    const postList = await BlogPost.find({}).sort({ _id: -1 }).populate('author')
     res.render('index', {postList})
 }
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -6,6 +6,7 @@ const BlogPostSchema = new Schema ({
     message: String,
     author: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true
     },
     datePosted: {
